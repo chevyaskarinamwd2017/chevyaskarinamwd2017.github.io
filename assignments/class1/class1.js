8ace8e5ece7e81fb9deae083d37f14860144858a
chevyaskarinamwd2017 <nyaskarinam@gmail.com>
2016-11-13 01:13:57 +0200

    uploading assignment 1

---
+++ assignments/class1/class1.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton").onclick = myFunction;
+
+function myFunction() {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
+}
+
+function myFunction() {
+    document.body.style.backgroundColor = "yellow";
+}
